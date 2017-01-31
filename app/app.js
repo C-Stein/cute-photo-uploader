e680c872c13dd03908a3f9f77432a924b9797091
Caitlin Stein <caitlin.teter@gmail.com>
2017-01-31 11:20:43 -0600

    user can upload a file and display a hardcoded file

--- app/app.js
+++ app/app.js
@@ -2,6 +2,16 @@ console.log('hi')
 
 const app = angular.module('cuteApp', ['ngRoute'])
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB_JTvdqgYthEXgBgnmAudkIoxoBNMvSTQ",
+    authDomain: "cute-animal-uploads.firebaseapp.com",
+    databaseURL: "https://cute-animal-uploads.firebaseio.com",
+    storageBucket: "cute-animal-uploads.appspot.com",
+    messagingSenderId: "971493546345"
+  };
+  firebase.initializeApp(config);
+
 app.config(($routeProvider, $locationProvider) => {
   $locationProvider.hashPrefix('')
   $routeProvider
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,33 @@
 app.controller('HomeCtrl', function(){
   console.log('home control!')
+
+  // Create a root reference
+let storageRef = firebase.storage().ref();
+
+let inputElement = document.getElementById("fileInput");
+
+inputElement.addEventListener("change", handleFiles, false);
+
+
+  function handleFiles() {
+    var fileList = this.files; /* now you can work with the file list */
+    console.log("filelist[0]", fileList[0])
+    storageRef.child(fileList[0].name).put(fileList[0])
+      .then(function(snapshot) {
+        console.log('Uploaded a blob or file!');
+      });
+  }
+
+//getting the url
+  storageRef.child('fox2.jpeg').getDownloadURL()
+    .then(function(url) {
+      var img = document.getElementById('myimg');
+      img.src = url;
+    }).catch(function(error) {
+    // Handle any errors
+  });
+
+
+
+
 })
